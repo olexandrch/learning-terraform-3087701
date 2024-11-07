952789355082c39078f928d66ec52a4f6e2bb4bc
Alex Chesnakov <olexandrchess@gmail.com>
2024-11-06 20:01:03 -0800

    Add ALB

--- main.tf
+++ main.tf
@@ -51,10 +51,10 @@ module "alb" {
   name            = "blog-alb"
   vpc_id          = module.blog_vpc.vpc_id
   subnets         = module.blog_vpc.public_subnets
-  security_groups = module.blog_sg.security_group_id
+  security_groups = [module.blog_sg.security_group_id]
 
   listeners = {
-    ex-http-https-redirect = {
+    http-listener = {
       port     = 80
       protocol = "HTTP"
     }
@@ -66,7 +66,7 @@ module "alb" {
       protocol         = "HTTP"
       port             = 80
       target_type      = "instance"
-      target_id        = aws_instance.blog.id      
+      target_id        = aws_instance.blog.id
     }
   }
 
