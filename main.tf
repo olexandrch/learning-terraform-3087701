3aabea83b75d3d4938a3826d63570bf6eb73a642
Alex Chesnakov <olexandrchess@gmail.com>
2024-11-06 20:16:04 -0800

    Add ALB, fix error

--- main.tf
+++ main.tf
@@ -45,45 +45,37 @@ resource "aws_instance" "blog" {
   }
 }
 
-module "alb" {
-  source = "terraform-aws-modules/alb/aws"
+module "blog_alb" {
+  source  = "terraform-aws-modules/alb/aws"
+  version = "~> 6.0"
 
-  name            = "blog-alb"
-  vpc_id          = module.blog_vpc.vpc_id
-  subnets         = module.blog_vpc.public_subnets
-  security_groups = [module.blog_sg.security_group_id]
+  name = "blog-alb"
 
-  listeners = {
-    ex-http = {
-      port     = 80
-      protocol = "HTTP"
-      
-      default_action = {
-        type = "forward"
-        target_group_key = "ex-instance"  # References the target group defined below
-      }
-    }
-  }
+  load_balancer_type = "application"
 
-  target_groups = {
-    ex-instance = {
+  vpc_id             = module.blog_vpc.vpc_id
+  subnets            = module.blog_vpc.public_subnets
+  security_groups    = [module.blog_sg.security_group_id]
+
+  target_groups = [
+    {
       name_prefix      = "blog-"
-      protocol         = "HTTP"
-      port             = 80
+      backend_protocol = "HTTP"
+      backend_port     = 80
       target_type      = "instance"
-      
-      targets = {
-        my_target = {
-          target_id = aws_instance.blog.id
-          port      = 80
-        }
-      }
     }
-  }
+  ]
+
+  http_tcp_listeners = [
+    {
+      port               = 80
+      protocol           = "HTTP"
+      target_group_index = 0
+    }
+  ]
 
   tags = {
     Environment = "dev"
-    Project     = "Blog"
   }
 }
 
